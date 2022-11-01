e6451b3031c3ca0165260ea569b32b54d62c841a
facund1n <facus16s@gmail.com>
2022-11-01 01:08:34 -0300

    FOlder Optimization

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="mt-auto py-4 text-center naranja">
+      <h2>BLOG SHOW - Todos los derechos reservados</h2>
+    </div>
+  );
+};
+
+export default Footer;
