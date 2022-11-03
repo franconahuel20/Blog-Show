e5c012ba9b743c78d6004045bd0506e493b22541
facund1n <facus16s@gmail.com>
2022-11-03 12:53:55 -0300

    footer + social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Social from "./Social";
+
 const Footer = () => {
   return (
-    <div className="mt-auto py-4 text-center naranja">
-      <h2>BLOG SHOW - Todos los derechos reservados</h2>
-    </div>
+    <>
+      <Container fluid className="mt-auto py-4 text-center gradient1">
+        <Row lg={6} sm={3} xs={1} className="justify-content-center">
+          <Col className="align-items-center">
+            <img alt="logo" src="/img/logo.ico" className="img-fluid me-auto" />
+          </Col>
+          <Col>
+            <Social />
+            <p>Política de privacidad</p>
+            <p>Condiciones de uso</p>
+          </Col>
+        </Row>
+        <Row>
+          <p>copyright - 2022</p>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -7,7 +7,11 @@ import { Container } from "react-bootstrap";
 const Main = () => {
   return (
     <>
-      <Container fluid className="px-0 d-flex flex-column min-vh-100">
+      <Container
+        fluid
+        className="px-0 d-flex flex-column min-vh-100"
+        style={{ backgroundColor: "#000000" }}
+      >
         <Header />
         <Landing />
         <Carousel />
