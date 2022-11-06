f1a809bcdf1197f3ccd2131b29fa75285c54280b
Franciscot93 <franciscot.p.93@gmail.com>
2022-11-06 13:36:56 -0300

    social icons in the nav udpate

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAt, faThumbsUp} from '@fortawesome/free-solid-svg-icons';
 import Badge from 'react-bootstrap/Badge';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 
@@ -14,11 +17,30 @@ export const NavBar = () => {
     <>
       <Navbar sticky='top' className='w-100' bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <div>
+              <div>
+                <img src="./img/logo.ico" alt="logo" />
+              </div>
+          
+                       
+              <div>
+                <img className='mx-1' src="./img/instasplash.png" alt="instagram" />
+              
+              
+                <img className='mx-1' src="./img/facebook-splash.png" alt="instagram" />
+              
+              
+                <img className='mx-1' src="./img/twitter-splash.png" alt="instagram" />
+              
+            
+              </div>
+              </div>
+            </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Destacado</Nav.Link>
+            <Nav.Link href="#features">Destacado</Nav.Link>
             <Nav.Link href="#features">Contacto</Nav.Link>
-            <Nav.Link href="#pricing"></Nav.Link>
+            
           </Nav>
           <Nav>
             <Nav.Link href="#deets"><FontAwesomeIcon icon={faThumbsUp} /><Badge className='mx-1' bg="secondary">New</Badge></Nav.Link>
